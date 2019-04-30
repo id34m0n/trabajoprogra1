f0911363926783693718625af2c444b3078b1844
mapacho <you@example.com>
2019-04-30 11:17:08 -0300

    Trabajo Progra1

---
+++ lista progra1.py
@@ -0,0 +1,35 @@
+def mas_larga(lista):
+    palabra = ""
+    largo = 0
+    for i in lista:
+        if (len(i) > largo):
+            palabra = i
+            largo = len(i)
+    return palabra
+
+print("A) Calcular palabra mas larga")
+print("")
+op= input("Ingrese Opcion")
+print("")
+
+lista = []
+
+if op == "A" or op == "a":
+    largo = int(input("Ingrese largo de la lista"))
+    print("")
+    for i in range(largo):
+        lista.append(input("Ingrese Palabra"))
+        print("")
+    print("La palabra mas larga es: ",mas_larga(lista))
+
+for i in lista:
+    invertido = i[::-1]
+    print(invertido)
+    
+    
+
+
+
+
+
+
